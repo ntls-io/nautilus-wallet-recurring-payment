605f8462449ce4f8354e6baa7d629bbc3bcdce39
Bill Guo <bill@ntls.io>
2023-07-05 09:21:59 +0200

    feat: add env variables for ntls-demo

--- recurring_payment_script.ts
+++ recurring_payment_script.ts
@@ -18,7 +18,7 @@ import { IssuedCurrencyAmount } from 'xrpl/dist/npm/models/common'
 const xrplService = new XrplService()
 const enclaveService = new EnclaveService(createAxiosInstance())
 
-const API_BASE_URL = 'https://wallet-staging-api.ntls.io'
+const API_BASE_URL = 'https://wallet-demo-api.ntls.io'
 const ISSUER = 'rpJv16Qmn2rQP6UC6UFsNRnVy5arkQihPP'
 
 // Function to create an instance of Axios with default configuration
