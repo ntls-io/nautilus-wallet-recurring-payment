6841afb3643faa3f1a650591aa160d8dabad8171
Bill Guo <bill@ntls.io>
2023-07-17 11:11:19 +0200

    refactor: duplicate recurring payment script for each env and add them to setup script

--- setup-cron.sh
+++ setup-cron.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
 ENV="env"
+ENV_NTLS_STAGING="ntls-staging"
+ENV_NTLS_DEMO="ntls-demo"
+ENV_PALAU_STAGING="palau-staging"
+ENV_PALAU_DEMO="palau-demo"
+ENV_BHUTAN_STAGING="bhutan-staging"
+ENV_BHUTAN_DEMO="bhutan-demo"
 
 # Path to the script
-TS_SCRIPT_PATH="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script.ts"
-JS_SCRIPT_PATH="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script.js"
+TS_SCRIPT_PATH_NTLS_STAGING="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_ntls_staging.ts"
+JS_SCRIPT_PATH_NTLS_STAGING="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_ntls_staging.js"
+
+TS_SCRIPT_PATH_NTLS_DEMO="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_ntls_demo.ts"
+JS_SCRIPT_PATH_NTLS_DEMO="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_ntls_demo.js"
+
+TS_SCRIPT_PATH_PALAU_STAGING="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_palau_staging.ts"
+JS_SCRIPT_PATH_PALAU_STAGING="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_palau_staging.js"
+
+TS_SCRIPT_PATH_PALAU_DEMO="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_palau_demo.ts"
+JS_SCRIPT_PATH_PALAU_DEMO="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_palau_demo.js"
+
+TS_SCRIPT_PATH_BHUTAN_STAGING="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_bhutan_staging.ts"
+JS_SCRIPT_PATH_BHUTAN_STAGING="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_bhutan_staging.js"
+
+TS_SCRIPT_PATH_BHUTAN_DEMO="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_bhutan_demo.ts"
+JS_SCRIPT_PATH_BHUTAN_DEMO="$HOME/nautilus-wallet-recurring-payment/recurring_payment_script_bhutan_demo.js"
 
 # Create the logs directory if it doesn't exist
 mkdir -p logs
 cd logs
-mkdir -p $ENV
+mkdir -p $ENV_NTLS_STAGING
+mkdir -p $ENV_NTLS_DEMO
+mkdir -p $ENV_PALAU_STAGING
+mkdir -p $ENV_PALAU_DEMO
+mkdir -p $ENV_BHUTAN_STAGING
+mkdir -p $ENV_BHUTAN_DEMO
 
 # Install Node.js
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &&\
@@ -19,19 +45,44 @@ sudo apt-get install -y nodejs
 npm install
 
 # Transpile TypeScript to JavaScript
-npx tsc "$TS_SCRIPT_PATH"
+npx tsc "$TS_SCRIPT_PATH_NTLS_STAGING"
+npx tsc "$TS_SCRIPT_PATH_NTLS_DEMO"
+npx tsc "$TS_SCRIPT_PATH_PALAU_STAGING"
+npx tsc "$TS_SCRIPT_PATH_PALAU_DEMO"
+npx tsc "$TS_SCRIPT_PATH_BHUTAN_STAGING"
+npx tsc "$TS_SCRIPT_PATH_BHUTAN_DEMO"
 
 # Add the cron job with the generated log file name
-CRON_JOB="0 7 * * * node $JS_SCRIPT_PATH >> $HOME/nautilus-wallet-recurring-payment/logs/$ENV/logfile_\`date +\%F+\%H:\%M:\%S\`.log 2>&1"
+CRON_JOB_NTLS_STAGING="0 7 * * * node $JS_SCRIPT_PATH_NTLS_STAGING >> $HOME/nautilus-wallet-recurring-payment/logs/$ENV_NTLS_STAGING/logfile_\`date +\%F+\%H:\%M:\%S\`.log 2>&1"
+CRON_JOB_NTLS_DEMO="0 7 * * * node $JS_SCRIPT_PATH_NTLS_DEMO >> $HOME/nautilus-wallet-recurring-payment/logs/$ENV_NTLS_DEMO/logfile_\`date +\%F+\%H:\%M:\%S\`.log 2>&1"
+CRON_JOB_PALAU_STAGING="0 7 * * * node $JS_SCRIPT_PATH_PALAU_STAGING >> $HOME/nautilus-wallet-recurring-payment/logs/$ENV_PALAU_STAGING/logfile_\`date +\%F+\%H:\%M:\%S\`.log 2>&1"
+CRON_JOB_PALAU_DEMO="0 7 * * * node $JS_SCRIPT_PATH_PALAU_DEMO >> $HOME/nautilus-wallet-recurring-payment/logs/$ENV_PALAU_DEMO/logfile_\`date +\%F+\%H:\%M:\%S\`.log 2>&1"
+CRON_JOB_BHUTAN_STAGING="0 7 * * * node $JS_SCRIPT_PATH_BHUTAN_STAGING >> $HOME/nautilus-wallet-recurring-payment/logs/$ENV_BHUTAN_STAGING/logfile_\`date +\%F+\%H:\%M:\%S\`.log 2>&1"
+CRON_JOB_BHUTAN_DEMO="0 7 * * * node $JS_SCRIPT_PATH_BHUTAN_DEMO >> $HOME/nautilus-wallet-recurring-payment/logs/$ENV_BHUTAN_DEMO/logfile_\`date +\%F+\%H:\%M:\%S\`.log 2>&1"
 
 # Write the cron job to a temporary file
-echo "$CRON_JOB" > /tmp/cronjob
+echo "$CRON_JOB_NTLS_STAGING" > /tmp/cronjob_1
+echo "$CRON_JOB_NTLS_DEMO" > /tmp/cronjob_2
+echo "$CRON_JOB_PALAU_STAGING" > /tmp/cronjob_3
+echo "$CRON_JOB_PALAU_DEMO" > /tmp/cronjob_4
+echo "$CRON_JOB_BHUTAN_STAGING" > /tmp/cronjob_5
+echo "$CRON_JOB_BHUTAN_DEMO" > /tmp/cronjob_6
 
 # Install the cron job
-crontab /tmp/cronjob
+crontab /tmp/cronjob_1
+crontab /tmp/cronjob_2
+crontab /tmp/cronjob_3
+crontab /tmp/cronjob_4
+crontab /tmp/cronjob_5
+crontab /tmp/cronjob_6
 
 # Remove the temporary file
-rm /tmp/cronjob
+rm /tmp/cronjob_1
+rm /tmp/cronjob_2
+rm /tmp/cronjob_3
+rm /tmp/cronjob_4
+rm /tmp/cronjob_5
+rm /tmp/cronjob_6
 
 echo "Cron job has been set up successfully."
 
